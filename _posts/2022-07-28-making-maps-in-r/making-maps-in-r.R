b64d6c6c0489fcf08541fcb82617759dd0898b8a
Kyle Belanger <kyleb44@hotmail.com>
2022-08-11 11:24:42 -0400

    Update making-maps-in-r.R

--- _posts/2022-07-28-making-maps-in-r/making-maps-in-r.R
+++ _posts/2022-07-28-making-maps-in-r/making-maps-in-r.R
@@ -5,6 +5,7 @@ cat("\f") # clear console
 # ---- load-packages --------------------------------------------------
 # Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
 library(tidyverse)
+library(ggmap)
 library(highcharter)
 
 
@@ -16,13 +17,44 @@ library(highcharter)
 
 
 
-
 # ---- load-data ------------------------------------------------------
 
 # load data from Tidy Tuesday 3//01/22
 # https://github.com/rfordatascience/tidytuesday/blob/master/data/2022/2022-03-01/readme.md
 
 ev_stations <- tidytuesdayR::tt_load(2022, week = 9) %>% .$stations
+
+ds1 <- ev_stations %>%
+  filter(STATE == "NC") %>%
+  select(LATITUDE, LONGITUDE, STATION_NAME, FUEL_TYPE_CODE, STATE, CITY) %>%
+  rename(lat = LATITUDE, lon = LONGITUDE, name = STATION_NAME)
+
+
+# ggmap -------------------------------------------------------------------
+
+# Using GGMAP to create map of NC EV Staions
+
+nc_boundries <- c(-85, 33.5, -75, 37)
+
+get_stamenmap(bbox = nc_boundries, zoom = 7, map = "toner") %>% ggmap() +
+  geom_point(data = ds1, aes(x = lon, y = lat, fill = FUEL_TYPE_CODE), shape = 21, size = 3) +
+  scale_fill_brewer(palette = "Dark2") +
+  theme_void() +
+  theme(legend.position = "bottom"
+        ,plot.title = element_text(hjust = 0.5, margin=margin(0,0,30,0)) #center title and add space due to theme
+        ) +
+  guides(  #use to move legend title to the top, default is to the side
+    fill = guide_legend(title.position = "top", title.hjust = 0.5)
+  ) +
+  labs(
+    title = "Alternate Fuel Stations in North Carolina"
+    ,fill = "Station Type"
+  )
+
+
+
+
+
 
 
 
@@ -55,9 +87,10 @@ hc1 <- hcmap("countries/us/us-nc-all", showInLegend = FALSE
     ,hcaes(color = color
            ,group = FUEL_TYPE_CODE
            )  #use this to add the color and group to each point
-  ,color =  colors
+  ,color =  colors  # use this to set the color of the series in the legend
   ) %>%
-  hc_title(text = "")
+  hc_title(text = "Alternate Fuel Stations in North Carolina")
+#note no legend title.  No default way to center legend title and I don't like the look of it
 
 
 hc1
